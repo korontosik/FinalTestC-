366c0e527dfec8497b6b4da6c9bc1d3b41c87b22
Andrew Koroti <korontos@mail.ru>
2024-05-27 12:32:08 +0300

    Add print

--- Task1.cs
+++ Task1.cs
@@ -2,23 +2,35 @@ string[] array = new string[] { "Hello", "2", "world", ":-)" };
 
 int newArrayLen = 0;
 
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i <= array.Length - 1; i++)
 {
     if (array[i].Length <= 3) newArrayLen++;
-
 }
+
 string[] newArray = new string[newArrayLen];
 int count = 0;
-for (int i = 0; i < array.Length - 1; i++)
+
+for (int i = 0; i <= array.Length - 1; i++)
 {
-    if (array[i].Length <=3){
+    if (array[i].Length <= 3)
+    {
         newArray[count] = array[i];
         count++;
     }
 }
 
 
-for (int i = 0; i < array.Length; i++)
+static void PrintArray(string[] array)
 {
-    Console.WriteLine(array[i]);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
 }
+
+
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray(newArray);
+
+
